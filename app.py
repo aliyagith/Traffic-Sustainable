dfb355b4fd71145f399c711d23eeff758e7de2b1
Aliya chauhan <aliyachauhan749@gmail.com>
2025-09-03 14:54:14 +0530

    ..

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 app.secret_key = 'traffic_secret_key'
 
+
+df = pd.read_csv('futuristic_city_traffic.csv')
+
+# Load the save model and preprocessing components
+model=joblib.load('traffic_model.pkl')
+scala = joblib.load('scaler.pkl')
+label_encoders = joblib.load('label_encoders.pkl')
+
+# Home route
 @app.route("/")
 def index():
-    return render_template('Index.html')
+    return render_template('index.html')
 
+@app.route("/predict")
+@login_required
+def predict():
+    return render_template('predict.html')
 
+# About route
 @app.route("/about")
 def about():
     return render_template('About.html')
